1d56a9b86c8475c0193b9091f726322db6f6c85e
Shreyansh <shreyansh.sahare@fyle.in>
2022-01-06 17:23:52 +0530

    Parameter changes and support for link account message

--- fyle_teams_app/views/handlers/command_handlers.py
+++ fyle_teams_app/views/handlers/command_handlers.py
@@ -1,10 +1,13 @@
 from typing import Dict
 
-from botbuilder.core import TurnContext
+from botbuilder.core import TurnContext, CardFactory
+from botbuilder.schema import Activity
 
 from django.http import JsonResponse
 
+from fyle_teams_app.libs import fyle_utils
 from fyle_teams_app.models import User, UserSubscription
+from fyle_teams_app.ui.cards import authorisation as authorisation_card
 
 
 class CommandHandler:
@@ -13,7 +16,8 @@ class CommandHandler:
 
     def initialize_command_handlers(self):
         self.command_handlers = {
-            'FYLE_UNLINK_ACCOUNT': self.handle_fyle_unlink_account
+            'UNLINK_FYLE_ACCOUNT': self.handle_unlink_fyle_account,
+            'LINK_FYLE_ACCOUNT': self.handle_link_fyle_account
         }
 
 
@@ -30,17 +34,19 @@ class CommandHandler:
 
         command_handler = self.command_handlers.get(command)
 
+        turn_context_dict = turn_context.activity.as_dict()
+
+        user_id = turn_context_dict['from_property']['id']
+
+        team_id = turn_context_dict['channel_data']['tenant']['id']
+
         if command_handler is not None:
-            return await command_handler(turn_context)
+            return await command_handler(turn_context, user_id, team_id)
 
         return JsonResponse({})
 
 
-    async def handle_fyle_unlink_account(self, turn_context: TurnContext) -> JsonResponse:
-
-        turn_context_dict = turn_context.activity.as_dict()
-
-        user_id = turn_context_dict['from_property']['id']
+    async def handle_unlink_fyle_account(self, turn_context: TurnContext, user_id: str, team_id: str) -> JsonResponse:
 
         user = await User.get_by_id(user_id)
 
@@ -49,6 +55,10 @@ class CommandHandler:
             message = 'You have not linked your Fyle account 🤕 '
 
         else:
+            processing_message = 'Hey we are unlinking your Fyle account with Microsft Teams, we will send a message once it is done'
+
+            await turn_context.send_activity(processing_message)
+
             await UserSubscription.disable_notification_subscriptions(user)
 
             await UserSubscription.remove_user_subscriptions(user_id)
@@ -58,5 +68,30 @@ class CommandHandler:
             message = 'You have successfully unlinked your Fyle account  🎊'
 
         await turn_context.send_activity(message)
+
+        return JsonResponse({})
+
+
+    async def handle_link_fyle_account(self, turn_context: TurnContext, user_id: str, team_id: str) -> JsonResponse:
+
+        user = await User.get_by_id(user_id)
+
+        if user.fyle_user_id is not None:
+
+            message = 'You have already linked your Fyle account 🎊'
+
+        else:
+
+            FYLE_OAUTH_URL = fyle_utils.get_fyle_oauth_url(user_id, team_id)
+
+            pre_auth_card = authorisation_card.get_pre_auth_card(FYLE_OAUTH_URL)
+
+            message = Activity(
+                attachments=[CardFactory.adaptive_card(pre_auth_card)]
+            )
+
+        await turn_context.send_activity(
+            message
+        )
 
         return JsonResponse({})
