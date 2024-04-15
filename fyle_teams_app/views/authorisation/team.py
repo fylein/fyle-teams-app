d94fa900e1df7af8f6e26d5a115bd3173c0d5007
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-04-15 20:04:29 +0530

    Added loggers for auth

--- fyle_teams_app/views/authorisation/team.py
+++ fyle_teams_app/views/authorisation/team.py
@@ -5,10 +5,11 @@ from botbuilder.schema import Activity
 from django.http.response import JsonResponse
 
 from fyle_teams_app.models import User
-from fyle_teams_app.libs import fyle_utils
+from fyle_teams_app.libs import fyle_utils, logger
 from fyle_teams_app.ui.cards import authorisation as authorisation_card
 
-logger = logging.getLogger(__name__)
+logger = logger.get_logger(__name__)
+logger.level = logging.INFO
 
 
 class TeamAuthorisation:
@@ -22,28 +23,28 @@ class TeamAuthorisation:
         team_id = turn_context_dict['channel_data']['tenant']['id']
         user_id = turn_context_dict['from_property']['id']
 
-        logger.error(f"User conversation reference: {user_conversation_reference_dict}")
-        logger.error(f"User id: {user_id}")
-        logger.error(f"Team id: {team_id}")
+        logger.info(f"User conversation reference: {user_conversation_reference_dict}")
+        logger.info(f"User id: {user_id}")
+        logger.info(f"Team id: {team_id}")
 
         # Check if user already exists
         user = await User.get_by_id(user_id)
 
         # If user exists -> user has already installed Fyle app
         if user is not None:
-            logger.error(f"User {user_id} has already installed Fyle app")
+            logger.info(f"User {user_id} has already installed Fyle app")
             return await turn_context.send_activity('Hey 👋, you\'ve already installed Fyle app!')
 
         # Create entry for user
         user = await User.create_user(team_id, user_id, user_conversation_reference_dict)
-        logger.error(f"User {user_id} created successfully")
+        logger.info(f"User {user_id} created successfully")
 
         FYLE_OAUTH_URL = fyle_utils.get_fyle_oauth_url(user_id, team_id)
-        logger.error(f"Fyle OAuth URL: {FYLE_OAUTH_URL}")
+        logger.info(f"Fyle OAuth URL: {FYLE_OAUTH_URL}")
         pre_auth_card = authorisation_card.get_pre_auth_card(FYLE_OAUTH_URL)
 
         user_details = await TeamsInfo.get_member(turn_context, user_id)
-        logger.error(f"User details: {user_details.as_dict()}")
+        logger.info(f"User details: {user_details.as_dict()}")
         User.track_bot_installation_status(user_details, 'Teams Bot Installed')
 
         return await turn_context.send_activity(
